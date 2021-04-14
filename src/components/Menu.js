6a7d729aded8633c239a922af66305f316015e48
D-coding-GH <daniel_hitchen@outlook.com>
2021-04-14 22:58:08 +0100

    hoover drop down menu added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,181 +1,168 @@
-import React, { Component,} from 'react';
+import React from "react";
+import { motion } from "framer-motion";
 import "./Menu.css"
 
+
 const Menu = () => {
+    const [isHover, toggleHover] = React.useState(false);
+    const toggleHoverMenu = () => {
+      toggleHover(!isHover);
+    };
+    const [isHover1, toggleHover1] = React.useState(false);
+    const toggleHoverMenu1 = () => {
+        toggleHover1(!isHover1);
+    };
+    const [isHover2, toggleHover2] = React.useState(false);
+    const toggleHoverMenu2 = () => {
+        toggleHover2(!isHover2);
+    };
+    const subMenuAnimate = {
+      enter: {
+        opacity: 1,
+        rotateX: 0,
+        transition: {
+          duration: 0.5
+        },
+        display: "block"
+      },
+      exit: {
+        opacity: 0,
+        rotateX: -15,
+        transition: {
+          duration: 0.5,
+          delay: 0.3
+        },
+        transitionEnd: {
+          display: "none"
+        }
+      }
+    };
     return (
-
-<div className="menuSpace" >
-        <div >
-            <Mens/>
+      <div className="App">
+        <div className="container">
+          <div className="flex-item">
+            <motion.div
+              className="menu-item"
+              onHoverStart={toggleHoverMenu}
+              onHoverEnd={toggleHoverMenu}
+            >
+              <h1>MEN</h1>
+              <motion.div
+                className="sub-menu"
+                initial="exit"
+                animate={isHover ? "enter" : "exit"}
+                variants={subMenuAnimate}
+              >
+                <div className="sub-menu-background" />
+                <div className="sub-menu-container">
+                <div className="titles1">
+                    <h1>FOOTWEAR</h1>
+                    <h1>BRAND</h1>
+                    <h1>COLLECTION</h1>
+                  </div>
+                  <div className="menuLinks">
+                    <div>
+                      <div>STREET</div>
+                      <div>ROAD</div>
+                      <div>TRAIL</div>
+                    </div>
+                    <div>
+                      <div>ADIDAS</div>
+                      <div>NIKE</div>
+                      <div>PUMA</div>
+                      <div>ASICS</div>
+                      <div>HOKA</div>
+                    </div>
+                    <div>
+                      <div>COLLLECTION</div>
+                      <div>COLLECTION</div>
+                    </div>
+                  </div>
+                  {/* <div className="sub-menu-item">Submenu Item 2</div>
+                  <div className="sub-menu-item">Submenu Item 3</div>
+                  <div className="sub-menu-item">Submenu Item 4</div>
+                  <div className="sub-menu-item">Submenu Item 5</div> */}
+                </div>
+              </motion.div>
+            </motion.div>
+          </div>
+          <div className="flex-item">
+            <motion.div
+              className="menu-item"
+              onMouseEnter={toggleHoverMenu1}
+              onMouseLeave={toggleHoverMenu1}
+            >
+              <h1>WOMEN</h1>
+              <motion.div
+                className="sub-menu1"
+                initial="exit"
+                animate={isHover1 ? "enter" : "exit"}
+                variants={subMenuAnimate}
+              >
+                <div className="sub-menu-background" />
+                <div className="sub-menu-container">
+                  <div className="titles1">
+                    <h1>FOOTWEAR</h1>
+                    <h1>BRAND</h1>
+                    <h1>COLLECTION</h1>
+                  </div>
+                  <div className="menuLinks">
+                    <div>
+                      <div>STREET</div>
+                      <div>ROAD</div>
+                      <div>TRAIL</div>
+                    </div>
+                    <div>
+                      <div>ADIDAS</div>
+                      <div>NIKE</div>
+                      <div>PUMA</div>
+                      <div>ASICS</div>
+                      <div>HOKA</div>
+                    </div>
+                    <div>
+                      <div>COLLLECTION</div>
+                      <div>COLLECTION</div>
+                    </div>
+                  </div>
+                  {/* <div className="sub-menu-item">Submenu Item 2</div>
+                  <div className="sub-menu-item">Submenu Item 3</div>
+                  <div className="sub-menu-item">Submenu Item 4</div>
+                  <div className="sub-menu-item">Submenu Item 5</div> */}
+                </div>
+              </motion.div>
+            </motion.div>
+          </div>
+          <div className="flex-item">
+            <motion.div
+              className="menu-item"
+              onMouseEnter={toggleHoverMenu2}
+              onMouseLeave={toggleHoverMenu2}
+            >
+              <h1>EQUIPMENT</h1>
+              <motion.div
+                className="sub-menu2"
+                initial="exit"
+                animate={isHover2 ? "enter" : "exit"}
+                variants={subMenuAnimate}
+              >
+                <div className="sub-menu-background" />
+                <div className="sub-menu-container">
+                <div className="titles1">
+                    <h1>EQUIPMENT</h1>
+                    <h1>EQUIPMENT</h1>
+                    <h1>EQUIPMENT</h1>
+                  </div>
+                </div>
+              </motion.div>
+            </motion.div>
+          </div>
         </div>
-        <div >
-             <Womens/>
-        </div>
-        <div  >
-             <Equipment/>
-        </div>
-</div>
+      </div>
     );
-};
+  }
 
 
 
-class Mens extends Component {
-
-    dropdownContainer = React.createRef();
-    state = {
-      open: false,
-    };
-    componentDidMount() {
-      document.addEventListener("mousedown", this.handleClickOutside);
-    }
-    componentWillUnmount() {
-      document.removeEventListener("mousedown", this.handleClickOutside);
-    }
-    handleClickOutside = (event) => {
-      if (
-        this.dropdownContainer.current &&
-        !this.dropdownContainer.current.contains(event.target)
-      ) {
-        this.setState({
-          open: false,
-        });
-      }
-    };
-    handleButtonClick = () => {
-      this.setState((state) => {
-        return {
-          open: !state.open,
-        };
-      });
-    };
-
-
-
-    render() {
-        return (
-            <div className="men">
-            <section className="menContainer" ref={this.dropdownContainer}>
-              <button id="regButton" onClick={this.handleButtonClick}>
-                MEN
-              </button>
-              {this.state.open && (
-                <div class="menContainer">
-                  <li className="showmenuli"> Street</li>
-                  <li className="showmenuli">Road</li>
-                  <li className="showmenuli">Trail</li>      
-                </div>
-              )}
-            </section>
-          </div>
-        );
-    }
-}
-
-
-class Womens extends Component {
-
-    dropdownContainer = React.createRef();
-    state = {
-      open: false,
-    };
-    componentDidMount() {
-      document.addEventListener("mousedown", this.handleClickOutside);
-    }
-    componentWillUnmount() {
-      document.removeEventListener("mousedown", this.handleClickOutside);
-    }
-    handleClickOutside = (event) => {
-      if (
-        this.dropdownContainer.current &&
-        !this.dropdownContainer.current.contains(event.target)
-      ) {
-        this.setState({
-          open: false,
-        });
-      }
-    };
-    handleButtonClick = () => {
-      this.setState((state) => {
-        return {
-          open: !state.open,
-        };
-      });
-    };
-
-
-
-    render() {
-        return (
-            <div className="women">
-            <section className="womenContainer" ref={this.dropdownContainer}>
-              <button id="regButton" onClick={this.handleButtonClick}>
-                Womens
-              </button>
-              {this.state.open && (
-                <div class="womenContainer">
-                  <li className="showmenuli"> Street</li>
-                  <li className="showmenuli">Road</li>
-                  <li className="showmenuli">Trail</li>      
-                </div>
-              )}
-            </section>
-          </div>
-        );
-    }
-}
-
-class Equipment extends Component {
-
-    dropdownContainer = React.createRef();
-    state = {
-      open: false,
-    };
-    componentDidMount() {
-      document.addEventListener("mousedown", this.handleClickOutside);
-    }
-    componentWillUnmount() {
-      document.removeEventListener("mousedown", this.handleClickOutside);
-    }
-    handleClickOutside = (event) => {
-      if (
-        this.dropdownContainer.current &&
-        !this.dropdownContainer.current.contains(event.target)
-      ) {
-        this.setState({
-          open: false,
-        });
-      }
-    };
-    handleButtonClick = () => {
-      this.setState((state) => {
-        return {
-          open: !state.open,
-        };
-      });
-    };
-
-
-
-    render() {
-        return (
-            <div className="equipment">
-            <section className="equipmentContainer" ref={this.dropdownContainer}>
-              <button id="regButton" onClick={this.handleButtonClick}>
-              Equipment
-              </button>
-              {this.state.open && (
-                <div class="equipmentContainer">
-                  <li className="showmenuli"> Street</li>
-                  <li className="showmenuli">Road</li>
-                  <li className="showmenuli">Trail</li>      
-                </div>
-              )}
-            </section>
-          </div>
-        );
-    }
-}
 
 
 export default Menu;
